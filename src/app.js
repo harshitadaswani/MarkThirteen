96d00e7943c3fc63f105dfa7934e4028af874aaa
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-18 04:54:22 +0530

    js logic added

--- src/app.js
+++ src/app.js
@@ -54,12 +54,117 @@ function checkPalindromeForAllDateFormats(date) {
   }
   return flag;
 }
-var date = {
-  day: 1,
-  month: 5,
-  year: 2001
-};
-console.log(checkPalindromeForAllDateFormats(date));
-// console.log(getDateInAllFormats(date));
+
+function isLeapYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getNextDate(date) {
+  var day = date.day + 1;
+  var month = date.month;
+  var year = date.year;
+  var monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (month === 2) {
+    if (isLeapYear(year)) {
+      if (day > 29) {
+        day = 1;
+        month++;
+      }
+    } else {
+      if (day > 28) {
+        day = 1;
+        month++;
+      }
+    }
+  } else {
+    if (day > monthDays[month - 1]) {
+      day = 1;
+      month++;
+    }
+  }
+  if (month > 12) {
+    month = 1;
+    year++;
+  }
+  return {
+    day: day,
+    month: month,
+    year: year
+  };
+}
+
+function getNextPalindromeDate(date) {
+  var counter = 0;
+  var nextDate = getNextDate(date);
+  while (1) {
+    counter++;
+    var isPalidromeDate = checkPalindromeForAllDateFormats(nextDate);
+    if (isPalidromeDate) {
+      break;
+    }
+    nextDate = getNextDate(nextDate);
+  }
+  return [counter, nextDate];
+}
+
+function getPreviousDate(date) {
+  var day = date.day - 1;
+  var month = date.month;
+  var year = date.year;
+  var monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (day === 0) {
+    month--;
+    if (month === 0) {
+      day = 31;
+      month = 12;
+      year--;
+    } else if (month === 2) {
+      if (isLeapYear(year)) {
+        day = 29;
+      } else {
+        day = 28;
+      }
+    } else {
+      day = monthDays[month - 1];
+    }
+  }
+  return {
+    day: day,
+    month: month,
+    year: year
+  };
+}
+
+function getPreviousPalindromeDate(date) {
+  var previousDate = getPreviousDate(date);
+  var counter = 0;
+  while (1) {
+    counter++;
+    var isPalidromeDate = checkPalindromeForAllDateFormats(previousDate);
+    if (isPalidromeDate) {
+      break;
+    }
+    previousDate = getPreviousDate(previousDate);
+  }
+  return [counter, previousDate];
+}
+
+// var date = {
+//   day: 01,
+//   month: 03,
+//   year: 2000
+// };
+
+//console.log(getPreviousPalindromeDate(date));
+//console.log(getPreviousDate(date));
+//console.log(getNextPalindromeDate(date));
+//console.log(getNextDate(date));
+//console.log(isLeapYear(date.year));
+//console.log(checkPalindromeForAllDateFormats(date));
+//console.log(getDateInAllFormats(date));
 //console.log(convertDateToString(date));
 //console.log(isPalindrome("racecar"));
